5e71000fafbfb0a0e432fa2a2b37d57df704e6c4
Admin <Admin@LAPTOP-C6GGMDAI>
2025-03-14 09:09:46 +0700

    update 3

--- src/GiaoDien/DoiMatKhau.java
+++ src/GiaoDien/DoiMatKhau.java
@@ -6,14 +6,14 @@ package GiaoDien;
 
 /**
  *
- * @author Administrator
+ * @author Admin
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class DoiMatKhau extends javax.swing.JFrame {
 
     /**
-     * Creates new form NewJFrame
+     * Creates new form DoiMatKhau
      */
-    public NewJFrame() {
+    public DoiMatKhau() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class NewJFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhau.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhau.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhau.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(DoiMatKhau.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new DoiMatKhau().setVisible(true);
             }
         });
     }
