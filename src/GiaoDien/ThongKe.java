29881f855c6655e7fe747587f04402645d3721e2
Admin <Admin@LAPTOP-C6GGMDAI>
2025-03-20 10:01:05 +0700

    update 7

--- src/GiaoDien/ThongKe.java
+++ src/GiaoDien/ThongKe.java
@@ -47,13 +47,13 @@ public class ThongKe extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null},
+                {null, null, null},
+                {null, null, null},
+                {null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Tháng", "Tổng số đơn hàng", "Tổng tiền bán ra"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
